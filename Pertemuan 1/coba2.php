87535d38946bbfbfaa324373fb066c0ecae9209a
MohLutfialfiansyah <114472690+MohLutfialfiansyah@users.noreply.github.com>
2023-02-24 15:49:16 +0700

    Add files via upload

---
+++ Pertemuan 1/coba2.php
@@ -0,0 +1,8 @@
+<?php
+
+   $tims = ['eko','ali','alfin','zoko'];
+
+   foreach($tims as $person){
+       echo $person.'<br>';
+   }
+   
---
+++ Pertemuan 1/index.php
@@ -0,0 +1,10 @@
+<?php
+  
+   $nama = "Muhammad Lutfi Alfian";
+   $Prodi = 'Teknik Informatika 13';
+   $Umur = 20;
+   $berat = 60;
+
+   echo 'Nama Saya Adalah:'.$nama;
+   echo '<br>Prodi saya adalah:'.$Prodi;
+   echo '<br>Umur saya adalah:'.$Umur;
